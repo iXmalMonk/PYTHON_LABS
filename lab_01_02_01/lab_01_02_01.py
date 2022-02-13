39b4a9e95b3357e4062650be3c61346ae0b46f3a
iXmalMonk <ixmal999@gmail.com>
2022-02-13 20:53:07 +0300

    Description

--- lab_01_02_01/lab_01_02_01.py
+++ lab_01_02_01/lab_01_02_01.py
@@ -2,3 +2,5 @@ p = float(input('P = '))
 k = float(input('K = '))
 t = float(input('T = '))
 print(f"H = {(p + k) ** 2 / p * k ** 0.5 / (k - p) / t ** 2:.3f}")
+
+# H = (p + k) ** 2 / p * k ** 0.5 / (k - p) / t ** 2
--- lab_01_02_03/lab_01_02_03.py
+++ lab_01_02_03/lab_01_02_03.py
@@ -2,3 +2,5 @@ import math
 x = float(input('X = '))
 y = float(input('Y = '))
 print(f"F = {math.atan((x + 1) ** (1 / 3) + math.cos(math.pi * x) / abs(x ** 2 + y)):.3f}")
+
+# F = math.atan((x + 1) ** (1 / 3) + math.cos(math.pi * x) / abs(x ** 2 + y))
