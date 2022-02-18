c0db2a5cffe5971049237b4dddb3468e3b5d1443
iXmalMonk <ixmal999@gmail.com>
2022-02-18 15:44:44 +0300

    labs

--- lab_01_02_01/lab_01_02_01.py
+++
@@ -1,6 +0,0 @@
-p = float(input('P = '))
-k = float(input('K = '))
-t = float(input('T = '))
-print(f"H = {(p + k) ** 2 / p * k ** 0.5 / (k - p) / t ** 2:.3f}")
-
-# H = (p + k) ** 2 / p * k ** 0.5 / (k - p) / t ** 2
--- lab_01_02_03_Manastyrskij_12/lab_01_02_03_Manastyrskij_12.py
+++ lab_01_02_03_Manastyrskij_12/lab_01_02_03_Manastyrskij_12.py
@@ -1,6 +1,9 @@
+# Вариант 12
+# Манастырский А.С.
+# Задача 1.2.3
+
 import math
-x = float(input('X = '))
-y = float(input('Y = '))
+x, y = map(float,input('Введите(через пробел) 2 числа (x, y) = ').split())
 print(f"F = {math.atan((x + 1) ** (1 / 3) + math.cos(math.pi * x) / abs(x ** 2 + y)):.3f}")
 
 # F = math.atan((x + 1) ** (1 / 3) + math.cos(math.pi * x) / abs(x ** 2 + y))
