b5dbfd200e9bd1a6e01674e87c039bdd719f9966
iXmalMonk <ixmal999@gmail.com>
2022-03-04 15:07:50 +0300

    fix

--- lab_03_05_Manastyrskij_12/lab_03_05_Manastyrskij_12.py
+++ lab_03_05_Manastyrskij_12/lab_03_05_Manastyrskij_12.py
@@ -1,11 +1,5 @@
 x, y = map(float, input("Введите X и Y = ").split())
-if x >= y ** 2 and y >= x ** 2:
-    print(f"Попадание при X = {x}\t Y = {y}")
-elif x <= -y ** 2 and y >= x ** 2:
-    print(f"Попадание при X = {x}\t Y = {y}")
-elif x >= y ** 2 and y <= -x ** 2:
-    print(f"Попадание при X = {x}\t Y = {y}")
-elif x <= -y ** 2 and y <= -x ** 2:
+if ((x >= y ** 2 or x <= -y ** 2) and y >= x ** 2) or ((x >= y ** 2 or x <= -y ** 2) and y <= -x ** 2):
     print(f"Попадание при X = {x}\t Y = {y}")
 else:
     print(f"Нет попадания при X = {x}\t Y = {y}")
