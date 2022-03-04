4f3dd20bc9a4eb8563f120b3848645f0bc537068
iXmalMonk <ixmal999@gmail.com>
2022-03-04 15:01:11 +0300

    fix lab

--- lab_03_05_Manastyrskij_12/lab_03_05_Manastyrskij_12.py
+++ lab_03_05_Manastyrskij_12/lab_03_05_Manastyrskij_12.py
@@ -1,11 +1,11 @@
 x, y = map(float, input("Введите X и Y = ").split())
-if x * y <= 1 and x > y ** 2 and y > x ** 2:
-    print(f"Попадание при X = {x}\t Y = {x}")
-elif x * y <= 1 and x < -y ** 2 and y > x ** 2:
-    print(f"Попадание при X = {x}\t Y = {x}")
-elif x * y <= 1 and x > y ** 2 and y < -x ** 2:
-    print(f"Попадание при X = {x}\t Y = {x}")
-elif x * y <= 1 and x < -y ** 2 and y < -x ** 2:
-    print(f"Попадание при X = {x}\t Y = {x}")
+if x >= y ** 2 and y >= x ** 2:
+    print(f"Попадание при X = {x}\t Y = {y}")
+elif x <= -y ** 2 and y >= x ** 2:
+    print(f"Попадание при X = {x}\t Y = {y}")
+elif x >= y ** 2 and y <= -x ** 2:
+    print(f"Попадание при X = {x}\t Y = {y}")
+elif x <= -y ** 2 and y <= -x ** 2:
+    print(f"Попадание при X = {x}\t Y = {y}")
 else:
-    print(f"Нет попадания при X = {x}\t Y = {x}")
+    print(f"Нет попадания при X = {x}\t Y = {y}")
